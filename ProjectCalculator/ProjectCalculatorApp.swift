ab49c1bd9111a0859208c23a2c1c9d586286819e
Victor-Zarzar <victorzarzar58@gmail.com>
2023-01-26 20:04:11 -0300

    Initial Commit

---
+++ ProjectCalculator/ProjectCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectCalculatorApp.swift
+//  ProjectCalculator
+//
+//  Created by Victor Zarzar on 26/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
